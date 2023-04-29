0e4fe422355ce357796bd90b37be34a95d14640c
actually-arnav <arnavsg@gmail.com>
2023-04-29 23:32:09 +0800

    Added circle wave, changed scrolling behaviour

--- sketch.js
+++ sketch.js
@@ -1,22 +1,12 @@
-/*
-
-battleplan:
-
-- changing cicle size and dist btwn circles and speed etc in app
-- true bounce of circles
-- when left click, initiate state where the particles move to other points
-- when right click, initiate state where particles move to beat of galaxy brain meme
-
-*/
-
 let pos;
 let bList;
-let r, s;
+let r, s, sList;
 let state;
 let counter;
 let diameter;
 let changing;
 let alpha;
+let change = 0;
 
 function setup() {
   //handling canvas size
@@ -44,13 +34,14 @@ function setup() {
   
   state = 0;
   
-  r = 50;
-  s = 1.2;
+  r = 0;
+  sList = [];
   
   counter = 0;
-  diameter = 60;
+  diameter = 20;
   changing = false;
   alpha = 0;
+  change = 0;
 }
 
 function draw() {
@@ -65,14 +56,17 @@ function draw() {
       num++;
     }
   }
-
   pos = [];
-  
+
   background(220);
+  
   push();
   translate(width/2, height/2);
-  r+=0.05;
-  s = (r-50) / 200 + 1.2;
+
+  push();
+
+  r += s < 0 ? -0.05 : 0.05;
+  s = r / 200 - 0.6;
   // s+=0.00005;
   rotate(r);
   scale(s);
@@ -80,14 +74,18 @@ function draw() {
     bList[p].jiggle();
   }
   pop();
-  
+
+  sineCircle();
+  pop();
+
   overlay();
 }
 
 function mouseWheel(event) {
-  diameter += event.deltaY/5;
+  diameter = abs(diameter) < 80 ? diameter + event.deltaY/5 : -diameter/abs(diameter) * 79.99;
   r -= event.deltaX/10;
-  alpha = alpha < 120 ? alpha+3 : 120;
+  s -= s/abs(s) * event.deltaX/20;
+  alpha = alpha < 170 ? alpha+3 : 170;
 }
 
 function scaleCanvas() {
@@ -101,5 +99,50 @@ function overlay() {
   fill(0, alpha);
   noStroke();
   rect(0, 0, width, height);
-  alpha = alpha > 0 ? (alpha-1)/1.1 : 0;
+  alpha = alpha > 0 ? (alpha-1)/1.2 : 0;
+}
+
+function sineCircle() {
+  push();
+
+  sList.push(s);
+  if (sList.length > 30) {
+    sList.shift();
+  }
+
+  scale(sList[0]);
+
+  let radius = width*6/11;
+
+  fill(190, 40, 40, 50);
+  beginShape();
+  for (let i=0; i<=360; i++) {
+    let l = (radius + radius/3 + 15*sin(i*9 + change));
+    let x = l * cos(i);
+    let y = l * sin(i);
+    curveVertex(x, y);
+  }
+  endShape(CLOSE);
+
+  beginShape();
+  for (let i=0; i<=360; i++) {
+    let l = (radius + 15*sin(i*9 + change*1.5+180));
+    let x = l * cos(i);
+    let y = l * sin(i);
+    curveVertex(x, y);
+  }
+  endShape(CLOSE);
+
+  beginShape();
+  for (let i=0; i<=360; i++) {
+    let l = (radius + radius/6 + 15*sin(i*9 + change*2+120));
+    let x = l * cos(i);
+    let y = l * sin(i);
+    curveVertex(x, y);
+  }
+  endShape(CLOSE);
+
+  change += s < 0 ? -1.5 : 1.5;
+
+  pop();
 }
